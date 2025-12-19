7df3cb0acfe055250eeac42c0377f9627b127a80
Jan Huus <jhuus1@gmail.com>
2025-12-19 15:24:56 -0500

    add occurrence_value method in OccurrenceDataProvider

--- src/britekit/occurrence_db/occurrence_data_provider.py
+++ src/britekit/occurrence_db/occurrence_data_provider.py
@@ -1,4 +1,7 @@
-# Defer some imports to improve initialization performance.
+from typing import Optional
+
+import numpy as np
+
 from britekit.core.exceptions import InputError
 from britekit.occurrence_db.occurrence_db import OccurrenceDatabase
 
@@ -26,6 +29,10 @@ class OccurrenceDataProvider:
         for county in self.counties:
             self.county_code_dict[county.code] = county.id
 
+        self.class_dict: dict[str, object] = {}
+        for _class in self.classes:
+            self.class_dict[_class.name] = _class
+
     def find_county(self, latitude: float, longitude: float):
         """
         Return county info for a given latitude/longitude, or None if not found.
@@ -48,6 +55,23 @@ class OccurrenceDataProvider:
 
         return None
 
+    def find_counties(self, region_code: str):
+        """
+        Return list of counties for a given region code.
+
+        Args:
+        - region_code (str): Region code, e.g. "CA", "CA-ON" or "CA-ON-OT".
+
+        Returns:
+            List of matching county objects.
+        """
+        counties = []
+        for county in self.counties:
+            if county.code.startswith(region_code):
+                counties.append(county)
+
+        return counties
+
     def smoothed_occurrences(self, county_code: str, class_name: str):
         """
         Return list of occurrence values for given county code and class name.
@@ -62,13 +86,15 @@ class OccurrenceDataProvider:
         """
         occurrences = self.occurrences(county_code, class_name)
         assert len(occurrences) == 0 or len(occurrences) == 48
-        for i in range(len(occurrences)):
-            occurrences[i] = max(
-                max(occurrences[i].value, occurrences[(i + 1) % 48].value),
-                occurrences[(i - 1) % 48].value,
-            )
 
-        return occurrences
+        smoothed = np.zeros(len(occurrences))
+        for i in range(len(occurrences)):
+            smoothed[i] = max(
+                max(occurrences[i], occurrences[(i + 1) % 48]),
+                occurrences[(i - 1) % 48],
+            ).item()
+
+        return smoothed
 
     def occurrences(self, county_code: str, class_name: str):
         """
@@ -104,8 +130,6 @@ class OccurrenceDataProvider:
         Returns:
             Numpy array of 48 average occurrence values (one per week, using 4-week months).
         """
-        import numpy as np
-
         total_values = np.zeros(48)
         zeros = np.zeros(48)
         total_weight = 0
@@ -166,3 +190,80 @@ class OccurrenceDataProvider:
                     max_value += 0
 
         return max_value / total_weight
+
+    def occurrence_value(
+        self,
+        class_name: str,
+        smoothed: bool = True,
+        region_code: Optional[str] = None,
+        latitude: Optional[float] = None,
+        longitude: Optional[float] = None,
+        week_num: Optional[int] = None,
+    ):
+        """
+        Given a class name, region code or latitude/longitude, and optional week number,
+        return the occurrence value for the given class/location/week.
+        Given a week and single county, return the value from smoothed occurrences.
+        Given a week and multiple counties, return the average smoothed value.
+        If no week is given, return the max value.
+
+        Args:
+        - class_name (str): Class name
+        - smoothed (bool): If true, use the max of adjacent week's values for each week.
+        - region_code (str, optional): Region code. If omitted, latitude/longitude must be provided.
+        - latitude (float, optional): Latitude
+        - longitude (float, optional): Longitude
+        - week_num (int, optional):
+
+        Returns:
+        - location_found (bool): True iff region/lat/lon map to a known county or counties
+        - class_found (bool): True iff class_name is in occurrence database
+        - occurrence (float): If location_found and class_found, occurrence value for given class/location/week, else None
+        """
+        import numpy as np
+
+        assert region_code is not None or (
+            latitude is not None and longitude is not None
+        )
+        if week_num is not None:
+            assert week_num >= 0 and week_num <= 47
+
+        location_found = True
+        if region_code is None:
+            assert latitude is not None and longitude is not None
+            county = self.find_county(latitude, longitude)
+            if county is None:
+                location_found = False
+            else:
+                counties = [county]
+        else:
+            counties = self.find_counties(region_code)
+            if len(counties) == 0:
+                location_found = False
+
+        class_found = class_name in self.class_dict
+        if not location_found or not class_found:
+            return location_found, class_found, None
+
+        if len(counties) == 1:
+            if smoothed:
+                occurrences = self.smoothed_occurrences(counties[0].code, class_name)
+            else:
+                occurrences = self.occurrences(counties[0].code, class_name)
+        else:
+            occurrence_lists = []
+            for county in counties:
+                if smoothed:
+                    occurrences = self.smoothed_occurrences(county.code, class_name)
+                else:
+                    occurrences = self.occurrences(county.code, class_name)
+
+                occurrence_lists.append(occurrences)
+
+            occurrences = np.array(occurrence_lists)
+            occurrences = np.mean(occurrences, axis=0)
+
+        if week_num is None:
+            return location_found, class_found, np.nanmax(occurrences).item()
+        else:
+            return location_found, class_found, occurrences[week_num].item()
