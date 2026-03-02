96954775320a8b41333c7e59febc1b91b5d8a83a
Jan Huus <jhuus1@gmail.com>
2026-03-02 17:16:01 -0500

    let last inference segment be as short as 1 second

--- src/britekit/core/predictor.py
+++ src/britekit/core/predictor.py
@@ -716,12 +716,11 @@ class Predictor:
             overlap = self.cfg.infer.overlap
 
         increment = max(0.5, self.cfg.audio.spec_duration - overlap)
-        end_offset = max(start_seconds, audio_duration - increment)
+        min_useful_audio = 1.0 # don't keep segments shorter than this
+        max_start = audio_duration - min_useful_audio
+        end_offset = max(start_seconds, max_start)
         start_times = util.get_range(start_seconds, end_offset, increment)
 
-        # Only keep start times that have at least 1 second of audio
-        min_useful_audio = 1.0
-        max_start = audio_duration - min_useful_audio
         return [t for t in start_times if t <= max_start]
 
     def _load_models(self, model_path: str) -> None:
